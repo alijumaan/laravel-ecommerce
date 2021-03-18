0648be64c13cec3890342647e7ff146630a2ebb8
ali <alila3883@gmail.com>
2021-03-18 11:15:15 +0300

    update "elliptic"

--- public/js/app.js
+++ public/js/app.js
@@ -527,8 +527,8 @@ eval("// removed by extract-text-webpack-plugin//# sourceURL=[module]\n//# sourc
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Library/WebServer/Documents/laravel/laravel-Ecommerce/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Library/WebServer/Documents/laravel/laravel-Ecommerce/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/ali/Desktop/Laravel/Laravel-Ecommerce/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/ali/Desktop/Laravel/Laravel-Ecommerce/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
