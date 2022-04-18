0d40ef70b65b763df73af179c37a7a982ea0d753
Ali Al Qahtani <73781540+alijumaan@users.noreply.github.com>
2022-04-18 22:33:22 +0300

    edit conterner in shop page

--- resources/views/frontend/shop/index.blade.php
+++ resources/views/frontend/shop/index.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Shop products')
 @section('content')
     <div class="shop-page-wrapper shop-page-padding ptb-100">
-        <div class="container-fluid m-auto">
+        <div class="container m-auto">
             <div class="row">
                 <div class="col-lg-3">
                     @include('partials.frontend.shop.sidebar')
